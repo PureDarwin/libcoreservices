62c46cf974bcc5c6b06ec2f3349fa1def041cc45
William Kent <wjk@users.noreply.github.com>
2018-11-10 15:42:14 -0500

    Use proper CrashReporterClient.h API
    I have found this API buried within the SPI folder
    of the WebKit2 Apple source package. The exact URL
    and license header are included.

--- CrashReporterClient/CrashReporterClient.c
+++ CrashReporterClient/CrashReporterClient.c
@@ -2,3 +2,15 @@
 
 char *__crashreporter_info__;
 asm(".desc __crashreporter_info__, 0x10");
+
+__attribute__((section("__DATA," CRASHREPORTER_ANNOTATIONS_SECTION)))
+struct crashreporter_annotations_t gCRAnnotations = {
+	.version = CRASHREPORTER_ANNOTATIONS_VERSION,
+	.message = 0,
+	.signature_string = 0,
+	.backtrace = 0,
+	.message2 = 0,
+	.thread = 0,
+	.dialog_mode = 0,
+	.abort_cause = 0
+};
