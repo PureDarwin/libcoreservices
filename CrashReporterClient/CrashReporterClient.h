1b151faa452f02ccc42759703c424800ee733c2f
William Kent <wjk011@gmail.com>
2020-05-28 22:18:30 -0400

    Add CRSetCrashLogMessage2()
    Also added CRGetCrashLogMessage2().

--- CrashReporterClient/CrashReporterClient.h
+++ CrashReporterClient/CrashReporterClient.h
@@ -39,6 +39,8 @@
 #define _crc_make_setter(attr, arg) (gCRAnnotations.attr = (uint64_t)(unsigned long)(arg))
 #define CRGetCrashLogMessage() _crc_make_getter(message)
 #define CRSetCrashLogMessage(m) _crc_make_setter(message, m)
+#define CRGetCrashLogMessage2() _crc_make_getter(message2)
+#define CRSetCrashLogMessage2(m) _crc_make_setter(message2, m)
 
 struct crashreporter_annotations_t {
 	uint64_t version;
